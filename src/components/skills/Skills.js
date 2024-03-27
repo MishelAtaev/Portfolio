09d19c580b28690ccc6c63bd12fd4e12ec10e98f
MishelAtaev <mishelataev@gmail.com>
2024-03-27 19:09:09 -0400

    updated icons

--- src/components/skills/Skills.js
+++ src/components/skills/Skills.js
@@ -1,10 +1,57 @@
 import React from "react";
-import "./Skills.css";
+import styled from "styled-components";
+import {
+  FaHtml5,
+  FaCss3Alt,
+  FaJsSquare,
+  FaNodeJs,
+  FaReact,
+} from "react-icons/fa";
+import { SiMongodb } from "react-icons/si";
+
+const StyledFaHtml5 = styled(FaHtml5)`
+  color: #e34f26;
+  font-size: 60px;
+`;
+
+const StyledFaCss3Alt = styled(FaCss3Alt)`
+  color: #1572b6;
+  font-size: 60px;
+`;
+
+const StyledFaJsSquare = styled(FaJsSquare)`
+  color: #f7df1e;
+  font-size: 60px;
+`;
+
+const StyledFaNodeJs = styled(FaNodeJs)`
+  color: #3c873a;
+  font-size: 60px;
+`;
+
+const StyledFaReact = styled(FaReact)`
+  color: #61dbfb;
+  font-size: 60px;
+`;
+
+const StyledSiMongodb = styled(SiMongodb)`
+  color: #4db33d;
+  font-size: 60px;
+`;
 
 const Skills = () => {
-    return( 
-        <div>Test</div>
-    );
+  return (
+    <section>
+      <div>
+        <StyledFaHtml5 />
+        <StyledFaCss3Alt />
+        <StyledFaJsSquare />
+        <StyledFaNodeJs />
+        <StyledFaReact />
+        <StyledSiMongodb />
+      </div>
+    </section>
+  );
 };
 
 export default Skills;
